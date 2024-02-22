e3588eaca8bd04d2bdbad8e219e160ebd8f13276
selint0 <selint@asu.edu>
2024-02-22 14:21:57 -0700

    added the last half to the  javascript

--- scripts.js
+++ scripts.js
@@ -20,4 +20,16 @@ for (let span of dkBlueSpans){
 
 let svgs = document.querySelectorAll("#svgs svg");
 
-let colors = ["",]
+let colors = ["#F2DCC9", "#D9BCA3", "#7C9EA6", "#3C5E73", "#283040"];
+
+for(let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+document.querySelector("#query .bold").style.color = "#7C9EA6";
+
+document.querySelector(".content_list li").textContent = "This is new list item text";
+
+document.querySelectorAll(".content_list li")[2].innerHTML += " <strong>Sam Lint</strong>";
+
+document.querySelector("#remove a").removeAttribute("hidden");
